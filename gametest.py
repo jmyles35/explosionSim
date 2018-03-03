d032dcc3980e07768353378a8c1a2afbada2d982
KeenanMcConkey <k.t.mcconkey@gmail.com>
2018-03-02 18:42:52 -0800

    Update gametest

--- gametest.py
+++ gametest.py
@@ -7,29 +7,32 @@ RED   = (255,   0,   0)
 GREEN = (  0, 255,   0)
 BLUE  = (  0,   0, 255)
 
-size = (500, 500)
+size = 500, 500
 screen = pygame.display.set_mode(size)
+
 pygame.display.set_caption("Explosion Test")
-
-done = False
-
 clock = pygame.time.Clock()
 
+done = False
+screen.fill(WHITE)
+
 while not done:
+    mouseClick = False
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouseClick = not mouseClick
 
-    # Game logic
+    if mouseClick:
+        pygame.draw.ellipse(screen, RED, [200, 200, 100, 100], 1)
 
-    # Drawing
+    font = pygame.font.SysFont('timesnewroman', 50, True, True)
+    text = font.render("Hello.", False, BLACK)
+    screen.blit(text, [190, 125])
 
-    screen.fill(WHITE)
     pygame.display.flip()
     clock.tick(60)
-
-    pygame.draw.ellipse(screen, BLACK, [20, 20, 250, 100], 2)
-    pygame.display.flip()
 
 pygame.quit()
