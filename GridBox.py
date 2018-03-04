0f01f92b210e2b49e1193b89e4b6910533ba7d95
Justin Myles <j.t.myles31@gmail.com>
2018-03-04 00:36:55 -0800

    some starter code'

---
+++ GridBox.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  3 17:01:09 2018
+
+@author: Marian
+"""
+
+
+import numpy as np
+import pandas as pd
+
+from scipy import stats, integrate
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+class GridBox:
+   def __init__(self, vx, vy, T, dens, pres):
+      self.vx = vx
+      self.vy = vy
+      self.T = T
+      self.dens = dens
+      self.pres = pres
