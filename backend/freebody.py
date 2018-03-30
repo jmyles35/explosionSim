4b5abbd7c3bdf8a3d9fc3181637de7a3f0b4eeca
esther lin <esther.lin@alumni.ubc.ca>
2018-03-30 15:03:28 -0700

    Updated Freebody to have object

--- backend/freebody.py
+++ backend/freebody.py
@@ -5,17 +5,35 @@ Created on Tue Mar 13 18:03:02 2018
 @author: Coleman
 """
 import numpy as np
-
-from numpy import matrix
-from numpy import linalg
+from numpy import matrix, linalg
 
 
-class freebody(S, xdim, ydim,StartPoint ):"
+class Freebody:
     """
     S=np.matrix([[1,0,0],[0,0,1],[0,1,1]])
     SP=0,0
     """
-        
+
+    def __init__(self, S, xdim, ydim, startPoint ):
+        """
+        This is the initialization/constructor function
+        for the Freebody function
+        """
+        #TODO: replace params with the parameters you want to take in
+
+        # Initilize the attributes of the object
+        self.S = S
+        self.xdim = xdim
+        self.ydim = ydim
+        self.startPoint = startPoint
+
+
+    def update(self, timestep, explosionMatrix):
+        """
+        Updates the blah blah blah...
+        """
+
+
     xlen,ylen=S.shape
     weight=0
     ColumnSum=np.sum(S,axis=0)
@@ -24,33 +42,20 @@ class freebody(S, xdim, ydim,StartPoint ):"
     for i in range(0,xlen):
         weight=weight+i*ColumnSum[0,i]
     xCOM=weight/Sum
-    
+
     weight=0
     RowSum=np.sum(S,axis=1)
     for i in range(0,xlen):
         weight=weight+i*RowSum[i,0]
     yCOM=weight/Sum
-    
-    def update (timstep, explosionMatrix, )
 
 
 
 
-               
-               
+
 
 """
-freebody takes 2 arrays of equally spaced x vaalues and y values"
-and the spacing between them. the values must come in pairs of 2 ex in for y we would get 
-the upper and lower limit of the object for a particular location in x 
+freebody takes 2 arrays of equally spaced x vaalues and y values
+and the spacing between them. the values must come in pairs of 2 ex in for y we would get
+the upper and lower limit of the object for a particular location in x
 and in x we would get the leftmost  and right most edge for a particular y"""
-
-
-                 
-        
-        
-        
-     
-     
-    
-    
